f8066ca1b693b9a128af6c6c828c6fe6129664e3
juanma <juanaco1.p@gmail.com>
2024-07-18 15:53:19 +0200

    select phone

--- background.js
+++ background.js
@@ -1,16 +1,30 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
-    id: 'openWithWhatsApp',
+    id: 'openWithWhatsAppLink',
     title: 'Open with WhatsApp Web',
     contexts: ['link'],
     targetUrlPatterns: ['tel:*']
   });
+
+  chrome.contextMenus.create({
+    id: 'openWithWhatsAppSelection',
+    title: 'Open with WhatsApp Web',
+    contexts: ['selection']
+  });
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === 'openWithWhatsApp') {
+  if (info.menuItemId === 'openWithWhatsAppLink') {
     const phone = info.linkUrl.replace('tel:', '').replace(/\s/g, '');
-    const waLink = `https://web.whatsapp.com/send/?phone=${phone}&text&type=phone_number`
+    const waLink = `https://web.whatsapp.com/send/?phone=${phone}&text&type=phone_number`;
     chrome.tabs.create({ url: waLink });
+  }
+
+  if (info.menuItemId === 'openWithWhatsAppSelection') {
+    const selectedText = info.selectionText.replace(/\s/g, '');
+    if (selectedText.match(/^\d+$/)) {
+      const waLink = `https://web.whatsapp.com/send/?phone=${selectedText}&text&type=phone_number`;
+      chrome.tabs.create({ url: waLink });
+    }
   }
 });
